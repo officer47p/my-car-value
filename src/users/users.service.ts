c3e7bec298b1c70c356c55cee66d338315175f12
Parsa Hosseini <officer47p@gmail.com>
2025-08-16 03:11:16 +0330

    Add update user logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -27,11 +27,17 @@ export class UsersService {
     async update(id: number, attrs: Partial<User>) {
         const user = await this.findOne(id)
         if (!user) {
-            throw new Error("user not found")
+            throw new NotFoundException("user not found")
         }
         Object.assign(user, attrs)
         return this.repo.save(user)
     }
 
-    remove() { }
+    async remove(id: number) {
+        const user = await this.findOne(id)
+        if (!user) {
+            throw new NotFoundException("user not found")
+        }
+        return this.repo.remove(user)
+    }
 }
