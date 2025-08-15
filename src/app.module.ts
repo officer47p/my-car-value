942610d9c33e8928d634237d75a1ef502896b0f9
Parsa Hosseini <officer47p@gmail.com>
2025-08-15 20:54:43 +0330

    Add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [User],
+    synchronize: true,
+  }), ReportsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
