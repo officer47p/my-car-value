e38d5d11902c4c2ddf8a212623a54a5bd1189bf3
Parsa Hosseini <officer47p@gmail.com>
2025-08-17 18:57:13 +0330

    Add auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -7,6 +7,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -22,6 +23,7 @@ export class UsersController {
     }
 
     @Get("/profile")
+    @UseGuards(AuthGuard)
     getProfile(@CurrentUser() user: User) {
         return user;
     }
