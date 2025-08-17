65307395e65a7719e693f03356269e3cafc6ec47
Parsa Hosseini <officer47p@gmail.com>
2025-08-17 04:08:30 +0330

    Implement signin and signup and signout functionalities

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -13,13 +13,27 @@ export class UsersController {
     }
 
     @Post("/signup")
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password)
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password)
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get("/profile")
+    getProfile(@Session() session: any) {
+        return this.usersService.findOne(session.userId)
+    }
+
+    @Post("/signout")
+    signOut(@Session() session: any) {
+        session.userId = null;
     }
 
     @Post("/signin")
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password)
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password)
+        session.userId = user.id;
+        return user;
     }
 
 
